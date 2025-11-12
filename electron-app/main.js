64d2b235fe6cc84899fb5e55f0c9dd3bea70957c
i32mibuf <i32mibuf@uco.es>
2025-11-12 14:22:56 +0100

    Cambio en main.js

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,12 +1,20 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 600,
-    height: 500,
-    webPreferences: { nodeIntegration: true, contextIsolation: false }
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
-  win.loadFile('renderer/index.html');
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
